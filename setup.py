e417ff51cac861aa869c73cfb6fe8c6dcf79048c
Matija Pavicevic <matija.pavicevic@kuleuven.be>
2019-11-06 09:43:34 +0100

    Environment and setup fix

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
-
 import codecs
 import os
-HERE = os.path.abspath(os.path.dirname(__file__))
+
+_here = os.path.abspath(os.path.dirname(__file__))
 
 def read(*parts):
     """
     Build an absolute path from *parts* and and return the contents of the
     resulting file.  Assume UTF-8 encoding.
     """
-    with codecs.open(os.path.join(HERE, *parts), "rb", "utf-8") as f:
+    with codecs.open(os.path.join(_here, *parts), "rb", "utf-8") as f:
         return f.read()
 
 # Sets the __version__ variable
@@ -19,10 +19,13 @@ exec(open('dispaset_sidetools/_version.py').read())
 setup(
     name='dispaset_sidetools',
     version=__version__,
+    description='Side tools for the Dispa-SET model ',
     author='Matija Pavičević, Sylvain Quoilin',
     author_email='matija.pavicevic@kuleuven.be',
-    description='Side tools for the Dispa-SET model ',
+    url='https://github.com/MPavicevic/DispaSET-SideTools',
+    license='EUPL v1.2',
     packages=find_packages(),
+    include_package_data=True,
     classifiers=[
         'Intended Audience :: Science/Research',
         'Programming Language :: Python'
