64395b7cb273163e2fc3e34467154f9366c6ee53
Damon <damon.coates@hotmail.com>
2020-03-18 06:37:38 +0100

    search updated with search_ElecLayer

--- dispaset_sidetools_final/get_demand/get_Demand_ES_hourly.py
+++ dispaset_sidetools_final/get_demand/get_Demand_ES_hourly.py
@@ -11,26 +11,60 @@ import pandas as pd
 import numpy as np
 import os
 from dispaset_sidetools.common import mapping,outliers,fix_na,make_dir
+from search import *
 #import matplotlib.pyplot as plt
 
 input_folder = '../../Inputs/'  # Standard input folder
 output_folder = '../../Outputs/'# Standard output folder
 
-#input file
-TotalLoadValue = pd.read_csv(input_folder + 'EUD_ELEC.txt',delimiter = '\t' ,index_col = 0)
 
-#Enter Studied Countries
-countries = ['BE']
 
+#Enter countries studied
+countries = list(['BE'])
+
+#Enter number of TD
+n_TD = 12
+
+#Enter technology names which elec consumption is to add to EUD
+tech = ['TRAMWAY_TROLLEY','TRAIN_PUB','TRAIN_CapitalfREIGHT'] #change list of tech to investigate - TO DO
+
+######################################################################################################
 #Enter the starting date
 date_str = '1/1/2015'
 start = pd.to_datetime(date_str)
 hourly_periods = 8760
 drange = pd.date_range(start, periods=hourly_periods, freq='H')
 
+#input file
+TotalLoadValue = pd.read_csv(input_folder + 'EUD_ELEC.txt',delimiter = '\t' ,index_col = 0) #EUD Load
 TotalLoadValue.set_index(drange, inplace=True)
 TotalLoadValue.columns = countries
 
+#keep only the percentage Tech_LT_heat_prod / CHP_LT_heat_prod + P2H_LT_heat_prod of this time series. put it back in heat_demand_ts for each country.
+tech_country = list()
+for Country in countries:
+    for y in tech:
+        elec = search_YearBalance(y, 'ELECTRICITY') #TO CHECK
+        if elec!=0 :
+            tech_country.append(Country + '_' + y)
+
+
+print(tech_country)
+
+TotalLoadValue_ESinput = pd.DataFrame(index=range(0,8760), columns=tech_country)
+
+for x in range(0,len(countries)):
+    for day in range(1,366):
+        print(day)
+        for h in range(1,25):
+            thistd = get_TD((day-1)*24+h,n_TD)
+            for y in tech:
+                name = countries[x] + '_' + y
+                TotalLoadValue_ESinput.at[(day-1)*24+h-1, name] = search_ElecLayers(thistd,h,y) * 1000 #TO CHECK
+
+TotalLoadValue_ESinput = TotalLoadValue_ESinput.sum(axis=1)
+TotalLoadValue = TotalLoadValue + TotalLoadValue_ESinput
+######################################################################################################
 
 def write_csv_files(file_name,demand,write_csv=None):
 
--- dispaset_sidetools_final/search.py
+++ dispaset_sidetools_final/search.py
@@ -50,6 +50,7 @@ def search_YearBalance(tech, feat):
     yearbal.set_index('Tech', inplace=True)
     techno = list(yearbal.index)
     techno2 = [x.strip(' ') for x in techno]
+    print(techno2)
     yearbal.set_index([techno2], inplace=True)
     output = yearbal.at[tech, feat]
 
@@ -93,6 +94,26 @@ def search_HeatLayers(type, TD, hour, tech):
     tech = ''.join(c for c in tech if c not in '-(){}<>[], ')
     tech = ''.join([i for i in tech if not i.isdigit()])
     output = HeatLayers.at[((TD-1)*24)+hour - 1, tech]
+    return output
+
+
+########################################################################################################################
+
+#
+#
+# Input :   TD = typical day studied
+#           hour = hour studied
+#           tech  = tech studied
+# Output :   Value of the feature asked
+#
+#
+def search_ElecLayers(TD, hour, tech):
+    input_folder = '../../Inputs/'  # input file = PowerPlant.csv (to find installed power f [GW or GWh for storage])
+    output_folder = '../../Outputs/'
+    ElecLayers = pd.read_csv(input_folder + 'ElecLayers.txt',delimiter='\t')
+    tech = ''.join(c for c in tech if c not in '-(){}<>[], ')
+    tech = ''.join([i for i in tech if not i.isdigit()])
+    output = ElecLayers.at[((TD-1)*24)+hour - 1, tech]
     return output
 
 ########################################################################################################################
