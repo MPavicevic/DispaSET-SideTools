84519c33c8e9047be87364e2beaaa8711aeda03c
evajoskin <ejoskin@hotmail.com>
2020-03-23 17:42:36 +0100

    Add H2 demand linked to Power-to-liquid + correct efficiency of electrolyzer

--- dispaset_sidetools/get_h2_demand/get_h2_demand_JRC_EU_TIMES.py
+++ dispaset_sidetools/get_h2_demand/get_h2_demand_JRC_EU_TIMES.py
@@ -6,6 +6,8 @@ This script generates the demand for H2 as an hourly time-series
 """
 
 import pandas as pd
+import sys,os
+sys.path.append(os.path.abspath(r'../..')) 
 from dispaset_sidetools.common import make_dir
 
 # %% Adjustable inputs that should be modified
@@ -14,6 +16,8 @@ WRITE_CSV_FILES = True  # Write csv database
 SCENARIO = 'ProRes1'  # Scenario name, used for naming csv files
 CASE = 'ALLFLEX'  # Case name, used for naming csv files
 SOURCE = 'JRC_EU_TIMES_'  # Source name, used for naming csv files
+EFF_DIESEL = 0.78 # Efficiency of hydrogenation to diesel
+EFF_METHANOL = 0.82 # Efficiency of hydrogenation to methanol
 
 # %% Inputs
 # Folder destinations
@@ -23,9 +27,27 @@ output_folder = '../../Outputs/'  # Standard output folder
 scenario = SCENARIO + '/'
 
 # %% Load data
-h2_total_demand = pd.read_excel(
+h2_normal_demand = pd.read_excel(
     input_folder + source_folder + scenario + 'TIMES_H2_Demand_2050.xlsx', 
     header=None, index_col = 0, skiprows = 2)
+h2_demand_PtL = pd.read_excel(
+    input_folder + source_folder + scenario + 'TIMES_H2_Demand_PtL_2050.xlsx', 
+    index_col = 0, skiprows = 1)
+
+# %% Correct H2 demand for PtL (what we have is demand for diesel and methanol, not for H2)
+for h in h2_demand_PtL.columns:
+    if "Diesel" in h:
+        h2_demand_PtL.loc[:,h] = h2_demand_PtL.loc[:,h]/EFF_DIESEL
+    elif "Methanol" in h:
+        h2_demand_PtL.loc[:,h] = h2_demand_PtL.loc[:,h]/EFF_METHANOL
+
+h2_demand_PtL_final=h2_normal_demand.copy()
+for c in h2_demand_PtL.index:
+    h2_demand_PtL_final.loc[c]=h2_demand_PtL.loc[c,:].sum() 
+    
+# %% Total demand for H2
+h2_total_demand = h2_normal_demand.copy()
+h2_total_demand[:] = h2_normal_demand[:] +  h2_demand_PtL_final[:] 
 
 # %% First solution: create a flat demand over the year
 dates = pd.date_range(start='1/1/2050', end='31/12/2050 23:00:00', freq='H')
