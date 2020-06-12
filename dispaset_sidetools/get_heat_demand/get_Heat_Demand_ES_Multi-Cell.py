fef412ef908329a3e893314cee53d528fc898a1e
damdamdeo3721 <49782819+damdamdeo3721@users.noreply.github.com>
2020-06-12 19:09:50 +0200

    Add files via upload

--- dispaset_sidetools/get_heat_demand/get_Heat_Demand_ES_Multi-Cell.py
+++ dispaset_sidetools/get_heat_demand/get_Heat_Demand_ES_Multi-Cell.py
@@ -32,11 +32,23 @@ drange = pd.date_range(start, periods=hourly_periods, freq='H')
 EUD_heat = {}
 
 for x in countries:
+    perc_dhn = perc_dhn_list[countries.index(x)]
+    DHN_Sto_losses = DHN_Sto_losses_list[countries.index(x)]
 
     #Input file
     heat_ESinput = from_excel_to_dataFrame(input_folder + x + '/' + 'DATA_preprocessing.xlsx', 'EUD_heat')
+    Distri_TS = pd.read_csv(input_folder + x + '/' + 'Distri_TS.txt', delimiter='\t')
+
+    #Compute ratio_dhn_prod_losses
+    dhn_sto_losses = Distri_TS['TS_DHN_SEASONAL'].sum() * DHN_Sto_losses
+    prod_dhn = 0
+    for i in dhn_tech:
+        prod_dhn = prod_dhn + search_YearBalance(x,i,'HEAT_LOW_T_DHN')
+    demand_dhn = heat_ESinput[x+'_LT'].sum() * perc_dhn /1000
+    ratio_dhn_prod_losses = 1+(prod_dhn-dhn_sto_losses-demand_dhn)/(prod_dhn-dhn_sto_losses)
+
     heat_ESinput[x+'_DEC'] = heat_ESinput[x+'_LT'].multiply(1-perc_dhn)
-    heat_ESinput[x+'_DHN'] = heat_ESinput[x+'_LT'].multiply(perc_dhn)
+    heat_ESinput[x+'_DHN'] = heat_ESinput[x+'_LT'].multiply(perc_dhn)*ratio_dhn_prod_losses
     heat_ESinput = heat_ESinput.drop([x+'_LT'], axis=1)
     heat_ESinput[x+'_IND'] = heat_ESinput[x+'_HT']
     heat_ESinput = heat_ESinput.drop([x+'_HT'], axis=1)
